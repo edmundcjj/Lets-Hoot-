2078cf307953b119525d2a87ccb52c29f26e7a50
Edmund <edmundcjj@gmail.com>
2018-03-15 14:16:50 +0800

    Added text animation and animation for the 4 options

--- js/index.js
+++ js/index.js
@@ -483,6 +483,45 @@ var quiz_questions = {
    console.log("Finished retrieving values of drop down list");
  }
 
+ // Function to start the text animation during display_game_details_state
+ function start_get_ready_text_animation(){
+   // Wrap every letter in a span
+  $('.ml11 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml11 .line',
+      scaleY: [0,1],
+      opacity: [0.5,1],
+      easing: "easeOutExpo",
+      duration: 700
+    })
+    .add({
+      targets: '.ml11 .line',
+      translateX: [0,$(".ml11 .letters").width()],
+      easing: "easeOutExpo",
+      duration: 700,
+      delay: 100
+    }).add({
+      targets: '.ml11 .letter',
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=775',
+      delay: function(el, i) {
+        return 34 * (i+1)
+      }
+    }).add({
+      targets: '.ml11',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+  }
+
  // Function to display game details before game starts
  function populate_game_details(){
    console.log("Populating game details now...");
@@ -1680,6 +1719,9 @@ function start_game(){
 
     // Set next state to be displayed - display_game_details_state
     toggle_state('set_game_details_state', 'display_game_details_state');
+
+    // Start the text animation for display_game_details_state
+    start_get_ready_text_animation();
 
     console.log("State has been toggled");
   });
